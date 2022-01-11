66b9cbce076394b8a6ee03efdb111f2c2c878eb1
MarcelinoBudiman <marcelinobudiman47@gmail.com>
2022-01-11 21:36:03 +0700

    add template

---
+++ Bibliotech/resources/views/admin/nav.blade.php
@@ -0,0 +1,24 @@
+@extends('template')
+
+@section('admin-nav')
+
+<nav class="navbar navbar-expand-md navbar-dark justify-content-center" style="background-color: {{PRIMARY_COLOR}};">
+        <div class="container-fluid">
+            <a class="navbar-brand me-auto p-2 bd-highlight" href="#">JH Furniture</a>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Borrow</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Queue</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About US</a>
+                </li>
+            </ul>
+    </nav>
+
+@endsection
