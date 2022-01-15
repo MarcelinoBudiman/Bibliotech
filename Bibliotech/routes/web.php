5a9625f6609efc78ca79a0b59ff21d42adbe2ee0
hezekiahcaleb <hezekiahcaleb85@gmail.com>
2022-01-15 22:18:03 +0700

    Finished book controller

--- Bibliotech/routes/web.php
+++ Bibliotech/routes/web.php
@@ -30,6 +30,15 @@ Route::post('/register', [UserController::class, 'storeUser']);
 
 //HOME
 Route::get('/home', [BookController::class, 'createHomePage']);
+Route::get('/search', [BookController::class, 'searchBook']);
+
+//BOOK
+Route::get('detail/{id}', [BookController::class, 'createDetailPage']);
+Route::get('add-book-page', [BookController::class, 'createAddPage']);
+Route::get('add-book', [BookController::class, 'insertBook']);
+Route::get('update-book-page/{id}', [BookController::class, 'createUpdatePage']);
+Route::get('update-page/{id}', [BookController::class, 'updateBook']);
+Route::get('delete-book/{id}', [BookController::class, 'deleteBook']);
 
 //BORROW
 Route::get('/borrow', function () {
